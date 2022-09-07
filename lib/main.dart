8e0a05e1907457a5bd11ca6de39fc2ed89225c5e
Arm <armm4sonic@gmail.com>
2022-09-07 13:01:38 +0700

    1th - Start UI Forniture

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:foniture_application/constants.dart';
+import 'package:foniture_application/models/product.dart';
+import 'package:foniture_application/screens/product/products_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Furniture App',
+      theme: ThemeData(
+        // We set Poppins as our default font
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+        primaryColor: kPrimaryColor,
+        //accentColor: kPrimaryColor,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ProductsScreen(),
+    );
+  }
+}
